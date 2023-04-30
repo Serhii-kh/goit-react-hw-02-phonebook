71695659ed06c1fbd3dd0a15ae466f085a1290cf
Serhii Naboka <drifterzzz@gmail.com>
2023-04-30 23:16:18 +0300

    add contacts array to the state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,8 +7,12 @@ import css from '../components/wrapper/wrapper.module.css';
 
 export class App extends Component {
 	state = {
-		contacts: [],
-
+		contacts: [
+			{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+			{ id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+			{ id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+			{ id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+		],
 		filter: ''
 	}
 
@@ -61,7 +65,6 @@ export class App extends Component {
 				<ContactForm onFormSubmit={this.addContact} />
 				<h2>Contacts</h2>
 				<Filter value={filter} onChange={this.changeFilter} />
-
 				<ContactsList contacts={filteredContacts} onDeleteContact={this.deleteContact} />
 			</div>
 		);
