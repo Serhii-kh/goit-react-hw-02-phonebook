8a5ae46bc4ca4f506dd5c29d2293fb1b8d0fa991
Serhii Naboka <drifterzzz@gmail.com>
2023-04-30 18:15:30 +0300

    light upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,17 +4,12 @@ import { ContactsList } from "./ContactsList/ContactsList";
 import { Filter } from "./Filter/Filter";
 import shortid from "shortid";
 import css from '../components/wrapper/wrapper.module.css';
-// import css from '../components/ContactsList/ContactsList.module.css';
-
 
 
 export class App extends Component {
 	state = {
 		contacts: [
-			{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-			{ id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-			{ id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-			{ id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+			
 		],
 
 		filter: ''
--- src/components/ContactsListItem/ContactsListItem.jsx
+++ src/components/ContactsListItem/ContactsListItem.jsx
@@ -1,14 +1,10 @@
-// import { Component } from 'react';
 import css from '../ContactsListItem/ContactsListItem.module.css'
 
-// handleDeleteContact = () => {}
+export const ContactsListItem = ({ name, number }) =>
+(<li className={css.ContactsListItem}>
+	<p>
+		{name}: {number}
+	</p>
+	<button type='button'>Delete</button>
+</li>)
 
-export const ContactsListItem  = ({name, number}) => 
-	(<li className={css.ContactsListItem}>
-			<p>
-				{name}: {number}
-			</p>
-			{/* <button type='button'>Delete</button> */}
-		</li>)
-
-// onClick = { handleDeleteContact }
