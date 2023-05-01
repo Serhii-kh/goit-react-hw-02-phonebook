655a6173c3e4a006f359607f289531b743d5deaa
Serhii Naboka <drifterzzz@gmail.com>
2023-05-01 12:56:35 +0300

    delete comments

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -18,31 +18,3 @@ ContactsList.propTypes = {
   contacts: PropTypes.array.isRequired,
   onDeleteContact: PropTypes.func.isRequired,
 };
-
-
-
-
-
-
-
-
-// import { ContactsListItem } from 'components/ContactsListItem/ContactsListItem';
-// import css from '../ContactsList/ContactsList.module.css';
-// import PropTypes from 'prop-types';
-
-// export const ContactsList = ({ contacts, onDeleteContact }) => (
-//   <ul className={css.contactsList}>
-//     {contacts.map(({ name, number, id }) => (
-//       <ContactsListItem key={id} name={name} number={number}>
-//         <button type="button" onClick={() => onDeleteContact(id)}>
-//           Delete
-//         </button>
-//       </ContactsListItem>
-//     ))}
-//   </ul>
-// );
-
-// ContactsList.propTypes = {
-//   contacts: PropTypes.array.isRequired,
-//   onDeleteContact: PropTypes.func.isRequired,
-// };
